292728d45ebe189a05e431094f659aa9d7e6c303
tchristeaan <tchristeaan@berkeley.edu>
2014-01-13 14:39:51 -0800

    first integration
    disk and peg prefabs and scripts, basic scene with three pegs

---
+++ Assets/DiskControls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiskControls : MonoBehaviour
+{
+
+		public int size = 1;
+
+		public int getSize ()
+		{
+				return size;
+		}
+		
+		public void setSize(int newSize) {
+			size = newSize;
+		transform.localScale = new Vector3(newSize,transform.localScale.y,newSize);
+		}
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
---
+++ Assets/PegControls.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PegControls : MonoBehaviour {
+
+
+	public Stack<DiskControls> disks = new Stack<DiskControls> ();
+	public bool start, destination;
+	public static int numDisks = 5;
+	static PegControls selected = null; 
+	public Transform diskFab;
+
+	void Start () {
+		if (start) {
+			for(int i = 0; i < numDisks; i++) {
+				DiskControls newDisk  = ((Transform) Instantiate(diskFab, new Vector3(transform.position.x,i - 0.5f,0), Quaternion.identity)).GetComponent<DiskControls>();
+				newDisk.setSize(numDisks+1 - i);
+				disks.Push(newDisk);
+			}
+		}
+		foreach (DiskControls dc in gameObject.GetComponentsInChildren<DiskControls>()) {
+			disks.Push(dc);
+			print (disks.Count);
+
+		}
+	}
+
+	void Select() {
+		selected = this;
+		transform.renderer.material.color = Color.green;
+	}
+
+	void DeSelect() {
+		selected = null;
+		transform.renderer.material.color = Color.gray;
+	}
+	void OnMouseDown() {
+		//if no tower has been selected and this peg is not empty, select this tower
+		if (selected == null) {
+			if(disks.Count > 0) {
+				Select();
+			}
+		//otherwise, transfer disk from selected peg
+		} else {
+			Transfer ();
+			selected.DeSelect ();
+		}
+	}
+
+	//Moves a disk from one peg to another.
+	void Transfer() {
+		if (selected.disks.Count > 0 && selected != this) {
+			print ("transfer!");
+			if (disks.Count == 0 || selected.disks.Peek ().getSize () < disks.Peek ().getSize ()) {
+				DiskControls moved = selected.disks.Pop ();
+				disks.Push (moved);
+				moved.transform.parent = transform;
+				moved.transform.position = new Vector3(transform.position.x,disks.Count-1.5f,0);
+			}
+		}
+	}
+}
